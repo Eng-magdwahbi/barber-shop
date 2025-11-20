262f4bd1bbf4ad8f1539609dea4fe7eac9d462e2
Eng-magdwahbi <eng.magdwahbe@gmail.com>
2025-11-20 23:16:25 +0200

    1234

--- src/components/2-MainPage/Services/Services.jsx
+++ src/components/2-MainPage/Services/Services.jsx
@@ -1,10 +1,12 @@
 import "./services.css";
 import Book from "../Book/Book";
 import ServicePage from "./ServicePage/ServicePage";
+
 export default function Services() {
   return (
-    <div className="main-service-container">
+    <div className="services-container">
       <ServicePage />
+
       <Book />
     </div>
   );
