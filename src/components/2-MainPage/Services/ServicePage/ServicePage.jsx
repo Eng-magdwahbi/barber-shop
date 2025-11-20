00f2ce08d662a338ec718f68f947922a9032c3f3
Mohamad-Wahbe <mohamadwahbe60@gmail.com>
2025-11-20 23:47:14 +0300

    Services

--- src/components/2-MainPage/Services/ServicePage/ServicePage.jsx
+++ src/components/2-MainPage/Services/ServicePage/ServicePage.jsx
@@ -1,4 +1,78 @@
 import "./servicepage.css";
+
+// Haircut Array
+const haircutArray = [
+  { name: "Haircut", time: "30", price: "40", img: "/images/15.png" },
+  {
+    name: "Haircut and Bread (Basic)",
+    time: "30",
+    price: "45",
+    img: "/images/30.png",
+  },
+  {
+    name: "Haircut and Bread (Premium)",
+    time: "30",
+    price: "50",
+    img: "/images/60.png",
+  },
+  {
+    name: "Wet Scissor Cut. (No Machine, Scissor Only)",
+    time: "30",
+    price: "55",
+    img: "",
+  },
+  { name: "Traditional Wet Shave", time: "20", price: "55", img: "" },
+  {
+    name: "Haircut and bread (Premium) + Bread Colour",
+    time: "45",
+    price: "60",
+    img: "",
+  },
+  { name: "Haircut and Wet Shave", time: "40", price: "85", img: "" },
+  { name: "Bread Only (Basic)", time: "15", price: "25", img: "" },
+  { name: "Bread Only (Premium)", time: "15", price: "30", img: "" },
+  { name: "Child Haircut (Under 16)", time: "25", price: "30", img: "" },
+  {
+    name: "Wet Scissor Cut Child (No Machine, Scissor Only)",
+    time: "30",
+    price: "40",
+    img: "",
+  },
+  { name: "Hair-line Only", time: "10", price: "20", img: "" },
+  { name: "Hair-line and Bread (Basic)", time: "20", price: "30", img: "" },
+  { name: "Hair-line and Bread (Premium)", time: "20", price: "35", img: "" },
+];
+
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 export default function ServicePage() {
-  return <div>ServicePage</div>;
+  return (
+    <div className="main-service-page">
+      <h2 className="title-service-page">Services</h2>
+
+      <div className="content-service-page">
+        {haircutArray.map((box) => {
+          return (
+            <div className="haircut">
+              <div className="img-des">
+                <div className="img">
+                  <img className="" src={box.img} />
+                </div>
+
+                <div className="des-haircut">
+                  <p>{box.name}</p>
+
+                  <div className="time-price">
+                    <p>{box.time} mins</p>
+                    <span className="small-circle"></span>
+                    <p>${box.price}</p>
+                  </div>
+                </div>
+              </div>
+              <ChevronRightIcon className="chevron-right-icon" />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
--- src/components/2-MainPage/Services/Services.jsx
+++ src/components/2-MainPage/Services/Services.jsx
@@ -1,5 +1,13 @@
 import "./services.css";
 import Book from "../Book/Book";
+import ServicePage from "./ServicePage/ServicePage";
+
 export default function Services() {
-  return <Book />;
+  return (
+    <div className="services-container">
+      <ServicePage />
+
+      <Book />
+    </div>
+  );
 }
